9f52e60c15c6760ef78b59629320858e4a78f9fa
kamil <kamil@LearniOSFreecoms-Mac.local>
2017-10-13 21:00:09 +0100

    Add views and actions

--- musicAlbum/ViewController.swift
+++ musicAlbum/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var album: [[String:Any]] = [];	
+    var album: [[String:Any]] = [];
+    var albumPtr: Int = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +23,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var getGenre: UITextField!
     @IBOutlet weak var getRelease: UITextField!
     @IBOutlet weak var getTracks: UITextField!
+    @IBOutlet weak var previousButton: UIButton!
     
+    @IBAction func toPreviousPage(_ sender: UIButton) {
+        self.albumPtr = self.albumPtr - 1;
+        if(albumPtr <= 0) {
+            self.albumPtr = 0;
+            previousButton.isEnabled = false;
+
+        }
+        self.getArtist.text = self.album[self.albumPtr]["artist"] as! String?
+        self.getTitle.text = self.album[self.albumPtr]["album"] as! String?
+        self.getGenre.text = self.album[self.albumPtr]["genre"] as! String?
+        let yearAlbum = self.album[self.albumPtr]["year"]!
+        let numTracks = self.album[self.albumPtr]["tracks"]!
+        self.getRelease.text = "\(yearAlbum)"
+        self.getTracks.text = "\(numTracks)"
+
+
+    }
+    @IBAction func toNextPage(_ sender: UIButton) {
+        self.albumPtr = self.albumPtr + 1;
+        if(albumPtr >= album.count) {
+            self.albumPtr = self.albumPtr - 1;
+            self.getArtist.text = ""
+            self.getTitle.text = ""
+            self.getGenre.text = ""
+            self.getRelease.text = ""
+            self.getTracks.text = ""
+        }
+        else {
+            self.previousButton.isEnabled = true;
+            self.getArtist.text = self.album[albumPtr]["artist"] as! String?
+            self.getTitle.text = self.album[albumPtr]["album"] as! String?
+            self.getGenre.text = self.album[albumPtr]["genre"] as! String?
+            let yearAlbum = self.album[albumPtr]["year"]!
+            let numTracks = self.album[albumPtr]["tracks"]!
+            self.getRelease.text = "\(yearAlbum)"
+            self.getTracks.text = "\(numTracks)"
+        }
+    }
+
+    @IBAction func deleteAction(_ sender: UIButton) {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -66,6 +110,7 @@ class ViewController: UIViewController {
                         let numTracks = self.album[0]["tracks"]!
                         self.getRelease.text = "\(yearAlbum)"
                         self.getTracks.text = "\(numTracks)"
+                        self.previousButton.isEnabled = false
                     }
                 } catch  {
                     print("error trying to convert data to JSON")
